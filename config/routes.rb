bb0aa7b97e2444697eae28d8140bc72d74daa820
Eben <ebenpelcyger@gmail.com>
2017-07-04 10:35:28 -0600

    Added update status functionality to blogs

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+     end
+  end
+
   root to: 'pages#home'
 end
