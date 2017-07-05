b6c209dcdcb166dc916428b91f23f065903c199c
Eben <ebenpelcyger@gmail.com>
2017-07-05 16:24:47 -0600

    Revert "Data feature"

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,10 +3,6 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end 
 
-  def angular
-     @angular_portfolio_items = Portfolio.angular 
-  end
-
   def new
     @portfolio_item = Portfolio.new
   end
--- app/models/topic.rb
+++
@@ -1,5 +0,0 @@
-class Topic < ApplicationRecord
-  validates_presence_of :title
-
-  has_many :blogs
-end
